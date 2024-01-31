93a4bb09640464bf14834f07fb8f348dbd547c0a
John <jhonmoorthi85131@gmail.com>
2024-01-31 16:26:57 +0530

    mob res

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -16,7 +16,7 @@ const List = ({ list }) => {
     setIsMoved(true);
     let distance = listRef.current.getBoundingClientRect().x - 50;
 
-    if (direction === "left" && slideNumber > 0) {
+    if (direction === "left" && slideNumber >= -1) {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
